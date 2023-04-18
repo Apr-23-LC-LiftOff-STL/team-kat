8839db4d56f1db0b610b827667cddbfdc67a7dc2
Nathan-422 <natiouswright@gmail.com>
2023-04-17 21:05:42 -0500

    init Spring server

---
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/TasteBuddiesServerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.TasteBuddiesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteBuddiesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteBuddiesServerApplication.class, args);
+	}
+
+}
