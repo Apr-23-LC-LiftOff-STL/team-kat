d174b6aad5ddccb0114453babc70979d44e9e70b
Nathan-422 <natiouswright@gmail.com>
2023-04-21 15:16:53 -0500

    Implemented UserRepository

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
@@ -1,2 +1,8 @@
-package org.launchcode.TasteBuddiesServer.data;public interface UserRepository {
+package org.launchcode.TasteBuddiesServer.data;
+
+import org.launchcode.TasteBuddiesServer.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
 }
