d648855d10d89eb87d3d78ccc91427ef871394e4
Nathan-422 <natiouswright@gmail.com>
2023-04-25 15:07:40 -0500

    Created security with basic http security config. Don't use that as it is not very secure.

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/SecurityConfiguration.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/SecurityConfiguration.java
@@ -1,17 +1,32 @@
 package org.launchcode.TasteBuddiesServer;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     // this is a basic start of a configuration. There is more to do here!
     // https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
