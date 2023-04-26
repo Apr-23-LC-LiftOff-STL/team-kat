a13bf13dc8ca91a7abfe4bf88f6dc437858526ab
Nathan-422 <natiouswright@gmail.com>
2023-04-25 20:26:46 -0500

    Added partially complete jwt filter

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/config/JwtAuthFilter.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/config/JwtAuthFilter.java
@@ -1,2 +1,57 @@
-package org.launchcode.TasteBuddiesServer.config;public class JwtAuthFilter {
+package org.launchcode.TasteBuddiesServer.config;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String email;
+        final String jwtToken;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authHeader.substring(7);
+        email = "Something"; // TODO to be implemented
+
+        // Check that there is an email and that the user is not already authenticated
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // TODO: Think about extending our User class with UserDetails as it will
+            // make Spring Security play nicely with authenticating our users
+            // This is a DB connection
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            final boolean isTokenValid; // TODO to be implemented
+            if (isTokenValid) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+
+
 }
