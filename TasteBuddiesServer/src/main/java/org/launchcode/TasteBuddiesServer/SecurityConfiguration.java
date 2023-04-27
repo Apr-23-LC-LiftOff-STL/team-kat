373fa35034d862755720446a21c6551160c2b995
Nathan-422 <natiouswright@gmail.com>
2023-04-27 14:47:27 -0500

    Added user service to SecurityConfiguration

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/SecurityConfiguration.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/SecurityConfiguration.java
@@ -2,47 +2,39 @@ package org.launchcode.TasteBuddiesServer;
 
 import lombok.RequiredArgsConstructor;
 import org.launchcode.TasteBuddiesServer.config.JwtAuthFilter;
+import org.launchcode.TasteBuddiesServer.data.UserRepository;
+import org.launchcode.TasteBuddiesServer.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.Optional;
+
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserRepository userRepository;
+
     private final JwtAuthFilter jwtAuthFilter;
     // this is a basic start of a configuration. There is more to do here!
     // https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
 
-
-//    This is the example authorization setup for overriding the SecurityFilterChain from
-//    the Spring Security tutorials.
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authz) -> authz
-//                        .anyRequest().authenticated()
-//                )
-//                .httpBasic(withDefaults());
-//        return http.build();
-//    }
-
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest()
@@ -70,5 +62,23 @@ public class SecurityConfiguration {
         return authenticationProvider;
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+
+            @Autowired UserRepository userRepository;
+
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<User> optionalUser = userRepository.findByEmail(username);
+
+                if (optionalUser.isPresent()) {
+                    return optionalUser.get();
+                } else {
+                    throw new UsernameNotFoundException("No user found with that username.");
+                }
+            }
+        };
+    }
 
 }
--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/AuthenticationController.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/AuthenticationController.java
@@ -1,114 +1,114 @@
-package org.launchcode.TasteBuddiesServer.controller;
-
-import org.launchcode.TasteBuddiesServer.data.UserRepository;
-import org.launchcode.TasteBuddiesServer.models.dto.LoginFormDTO;
-import org.launchcode.TasteBuddiesServer.models.dto.RegistrationFormDTO;
-import org.launchcode.TasteBuddiesServer.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-import java.io.IOException;
-import java.util.Optional;
-
-@Controller
-public class AuthenticationController {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    private static final String userSessionKey = "user";
-
-    private static void setUserInSession(HttpSession session, User user) {
-        session.setAttribute(userSessionKey, user.getId());
-    }
-
-    public User getUserFromSession(HttpSession session) {
-
-        Integer userId = (Integer) session.getAttribute(userSessionKey);
-        if (userId == null) {
-            return null;
-        }
-
-        Optional<User> userOpt = userRepository.findById(userId);
-
-        if (userOpt.isEmpty()) {
-            return null;
-        }
-
-        return userOpt.get();
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<?> processRegistrationForm(@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO,
-                                                  Errors errors,
-                                                  HttpServletRequest request,
-                                                  HttpServletResponse response) throws IOException {
-
-        if (errors.hasErrors()) {
-            // TODO figure out how to return a redirect page.
-            response.sendRedirect("/register");
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-        }
-
-        User existingUser  = userRepository.findByEmail(registrationFormDTO.getEmail());
-
-        if (existingUser != null) {
-            errors.rejectValue("email", "email.alreadyExists", "A account with that email already exists.");
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-        }
-
-        String password = registrationFormDTO.getPassword();
-        String verifyPassword = registrationFormDTO.getVerifyPassword();
-
-        if (password.equals(verifyPassword)) {
-            errors.rejectValue("password", "password.mismatch", "The entered passwords do not match.");
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-        }
-
-        User newUser = new User(registrationFormDTO.getUsername(), registrationFormDTO.getEmail(), registrationFormDTO.getPassword());
-        userRepository.save(newUser);
-        setUserInSession(request.getSession(), newUser);
-        response.sendRedirect("/login");
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
-                                              Errors errors,
-                                              HttpServletRequest request,
-                                              HttpServletResponse response) throws IOException {
-
-        if (errors.hasErrors()) {
-            response.sendRedirect("/login");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        User theUser = userRepository.findByEmail(loginFormDTO.getEmail());
-
-        String password = loginFormDTO.getPassword();
-
-        if (theUser == null || !theUser.isMatchingPassword(password)) {
-            errors.rejectValue(
-                    "password",
-                    "login.invalid",
-                    "Invalid credentials.");
-            response.sendRedirect("/login");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        setUserInSession(request.getSession(), theUser);
-        response.sendRedirect("/");
-        return new ResponseEntity<>(HttpStatus.OK);
-
-    }
-
-}
+//package org.launchcode.TasteBuddiesServer.controller;
+//
+//import org.launchcode.TasteBuddiesServer.data.UserRepository;
+//import org.launchcode.TasteBuddiesServer.models.dto.LoginFormDTO;
+//import org.launchcode.TasteBuddiesServer.models.dto.RegistrationFormDTO;
+//import org.launchcode.TasteBuddiesServer.models.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.Errors;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import javax.validation.Valid;
+//import java.io.IOException;
+//import java.util.Optional;
+//
+//@Controller
+//public class AuthenticationController {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    private static final String userSessionKey = "user";
+//
+//    private static void setUserInSession(HttpSession session, User user) {
+//        session.setAttribute(userSessionKey, user.getId());
+//    }
+//
+//    public User getUserFromSession(HttpSession session) {
+//
+//        Integer userId = (Integer) session.getAttribute(userSessionKey);
+//        if (userId == null) {
+//            return null;
+//        }
+//
+//        Optional<User> userOpt = userRepository.findById(userId);
+//
+//        if (userOpt.isEmpty()) {
+//            return null;
+//        }
+//
+//        return userOpt.get();
+//    }
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<?> processRegistrationForm(@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO,
+//                                                  Errors errors,
+//                                                  HttpServletRequest request,
+//                                                  HttpServletResponse response) throws IOException {
+//
+//        if (errors.hasErrors()) {
+//            // TODO figure out how to return a redirect page.
+//            response.sendRedirect("/register");
+//            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+//        }
+//
+//        User existingUser  = userRepository.findByEmail(registrationFormDTO.getEmail());
+//
+//        if (existingUser != null) {
+//            errors.rejectValue("email", "email.alreadyExists", "A account with that email already exists.");
+//            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+//        }
+//
+//        String password = registrationFormDTO.getPassword();
+//        String verifyPassword = registrationFormDTO.getVerifyPassword();
+//
+//        if (password.equals(verifyPassword)) {
+//            errors.rejectValue("password", "password.mismatch", "The entered passwords do not match.");
+//            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+//        }
+//
+//        User newUser = new User(registrationFormDTO.getUsername(), registrationFormDTO.getEmail(), registrationFormDTO.getPassword());
+//        userRepository.save(newUser);
+//        setUserInSession(request.getSession(), newUser);
+//        response.sendRedirect("/login");
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+//                                              Errors errors,
+//                                              HttpServletRequest request,
+//                                              HttpServletResponse response) throws IOException {
+//
+//        if (errors.hasErrors()) {
+//            response.sendRedirect("/login");
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//
+//        User theUser = userRepository.findByEmail(loginFormDTO.getEmail());
+//
+//        String password = loginFormDTO.getPassword();
+//
+//        if (theUser == null || !theUser.isMatchingPassword(password)) {
+//            errors.rejectValue(
+//                    "password",
+//                    "login.invalid",
+//                    "Invalid credentials.");
+//            response.sendRedirect("/login");
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//
+//        setUserInSession(request.getSession(), theUser);
+//        response.sendRedirect("/");
+//        return new ResponseEntity<>(HttpStatus.OK);
+//
+//    }
+//
+//}
