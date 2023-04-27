e4b89333f5f04db966bdf0d2432ed56a3f3108e4
Nathan-422 <natiouswright@gmail.com>
2023-04-26 22:59:51 -0500

    Updated models with Spring Security interfaces

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/models/AuthorityEntity.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/models/AuthorityEntity.java
@@ -1,2 +1,23 @@
-package org.launchcode.TasteBuddiesServer.models;public class Authority {
+package org.launchcode.TasteBuddiesServer.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class AuthorityEntity implements GrantedAuthority {
+
+    @Id
+    private String authority;
+
+    public AuthorityEntity(String role) {
+        this.authority = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+
 }
--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/models/User.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/models/User.java
@@ -1,12 +1,21 @@
 package org.launchcode.TasteBuddiesServer.models;
 
+import org.hibernate.collection.internal.PersistentSet;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-public class User extends AbstractEntity {
+public class User extends AbstractEntity implements UserDetails {
 
     // TODO add validation for displayName
     @NotNull
@@ -17,30 +26,69 @@ public class User extends AbstractEntity {
     private String email;
 
     @NotNull
-    private String passwordHash;
+    private String password;
+
+    @OneToMany()
+    private final Set<AuthorityEntity> authorites = new HashSet<>();
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public User() {
-
-    }
+    public User() { }
 
     public User(String displayName, String email, String password) {
         this.displayName = displayName;
         this.email = email;
-        this.passwordHash = encoder.encode(password);
+        this.password = encoder.encode(password);
     }
 
     public String getDisplayName() {
         return displayName;
     }
 
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
     public String getEmail() {
         return email;
     }
 
-    public boolean isMatchingPassword(String password) {
-        return encoder.matches(password, this.passwordHash);
+    @Override
+    public Set<AuthorityEntity> getAuthorities() {
+        return authorites;
     }
 
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, this.getPassword());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
