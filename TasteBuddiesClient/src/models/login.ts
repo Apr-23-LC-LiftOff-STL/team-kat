e9ea7f4ec2e5316caaaf2f501e4296f1184f4f3b
Nathan-422 <natiouswright@gmail.com>
2023-05-03 17:21:46 -0500

    modified login.ts

--- TasteBuddiesClient/src/models/login.ts
+++ TasteBuddiesClient/src/models/login.ts
@@ -1,10 +1,14 @@
 export class Login {
 
-    constructor(
-        public email: string,
-        public password: string,
-    ) {
+    email: string;
+    password: string
 
+    constructor(
+        email: string,
+        password: string,
+    ) {
+        this.email = email;
+        this.password = password;
     }
 
 }
