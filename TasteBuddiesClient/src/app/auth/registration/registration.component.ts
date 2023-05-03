3ece4245c007f2d474d8c29e856bbf828a76ed8d
Nathan-422 <natiouswright@gmail.com>
2023-05-02 20:15:22 -0500

    start of registration form

---
+++ TasteBuddiesClient/src/app/auth/registration/registration.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
