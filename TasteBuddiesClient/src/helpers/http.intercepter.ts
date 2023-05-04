e8a0e15d3508856d0b68b0abcf156c06e96168a4
Nathan-422 <natiouswright@gmail.com>
2023-05-04 18:43:20 -0500

    broke http interceptor

--- TasteBuddiesClient/src/helpers/http.intercepter.ts
+++ TasteBuddiesClient/src/helpers/http.intercepter.ts
@@ -1,17 +1,34 @@
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { StorageService } from "src/services/storage.service";
 
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+
+    constructor(private storageService: StorageService) {}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        
+        const idToken = this.storageService.getJwt();
+
+        if (idToken) {
+            req = req.clone({
+                headers: req.headers.set("Authorization", "Bearer " + idToken),
+                withCredentials: true,
+            });
+        } 
+        
         req = req.clone({
             withCredentials: true,
         });
 
         return next.handle(req);
     }
+
+    
+
 }
 
 export const httpInterceptorProviders = [
