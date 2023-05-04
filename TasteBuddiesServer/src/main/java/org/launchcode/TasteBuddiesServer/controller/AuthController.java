097ea5b60627b8d9bd2e6211d9b5b05b287ea114
Nathan-422 <natiouswright@gmail.com>
2023-05-04 11:26:44 -0500

    configured allowing credentials at the controller level

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/AuthController.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/AuthController.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class AuthController {
 
     @Autowired
