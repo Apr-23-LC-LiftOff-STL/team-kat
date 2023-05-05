e99bfa0adc4a53d53e3731b7df6c5752852c9632
Nathan-422 <natiouswright@gmail.com>
2023-05-05 17:52:17 -0500

    init home component

--- TasteBuddiesClient/src/app/app.module.ts
+++ TasteBuddiesClient/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { LoginComponent } from './auth/login/login.component';
 
 import { httpInterceptorProviders } from 'src/helpers/http.intercepter';
 import { AuthWalkthroughComponent } from './auth/auth-walkthrough/auth-walkthrough.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
     LoginComponent,
-    AuthWalkthroughComponent
+    AuthWalkthroughComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
