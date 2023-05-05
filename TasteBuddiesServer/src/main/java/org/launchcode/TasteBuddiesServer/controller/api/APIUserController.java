efb9de60f75efd2913e0135d4354e00d3dc91761
Nathan-422 <natiouswright@gmail.com>
2023-05-05 09:59:49 -0500

    modified dependencies

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/api/APIUserController.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/api/APIUserController.java
@@ -4,12 +4,8 @@ import org.launchcode.TasteBuddiesServer.data.UserRepository;
 import org.launchcode.TasteBuddiesServer.models.User;
 import org.launchcode.TasteBuddiesServer.models.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
