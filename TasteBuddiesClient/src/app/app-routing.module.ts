d7a58c1e66fd523697758b7e6d48aad9c4760bd9
Nathan-422 <natiouswright@gmail.com>
2023-05-05 18:33:20 -0500

    Added routing to home, login, and registration

--- TasteBuddiesClient/src/app/app-routing.module.ts
+++ TasteBuddiesClient/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
