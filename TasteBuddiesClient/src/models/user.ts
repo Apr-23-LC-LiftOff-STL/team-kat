93effd2952b99546f869a2aa7a80095d1d741416
Nathan-422 <natiouswright@gmail.com>
2023-05-07 17:13:33 -0500

    added user model

---
+++ TasteBuddiesClient/src/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id: number;
+    email: String;
+    displayName: String;
+
+    constructor(id: number, email: String, displayName: String) {
+        this.id = id;
+        this.email = email;
+        this.displayName = displayName;
+    }
+
+}
