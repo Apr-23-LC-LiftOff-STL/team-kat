fbe11510a2788c8187cd3ef5ae2f6375dc98b1cf
Nathan-422 <natiouswright@gmail.com>
2023-05-07 21:22:02 -0500

    Added auth guard service to restrict routes from unauthorized requests

--- TasteBuddiesClient/src/app/app-routing.module.ts
+++ TasteBuddiesClient/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { RegistrationComponent } from './auth/registration/registration.componen
 import { AccountComponent } from './account/account.component';
 import { UserEventsComponent } from './user-events/user-events.component';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+import { AuthGuardService as AuthGuard } from 'src/services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', title: 'Taste Buddies', component: HomeComponent },
   { path: 'login', title: 'Login', component: LoginComponent },
   { path: 'signup', title: 'Register', component: RegistrationComponent },
-  { path: 'account', title: 'Account', component: AccountComponent },
-  { path: 'event', title: 'Events', component: UserEventsComponent },
+  { path: 'account', title: 'Account', component: AccountComponent, canActivate: [AuthGuard] },
+  { path: 'event', title: 'Events', component: UserEventsComponent, canActivate: [AuthGuard] },
   { path: '**', title: 'Page not found', component: PageNotFoundComponent },
 ];
 
