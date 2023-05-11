6f1d1fcd4d6574ef5b1f9de38c0a0b407af896a0
Nathan-422 <natiouswright@gmail.com>
2023-05-11 14:33:30 -0500

    Added a simple repo method to search usernames

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
@@ -1,12 +1,20 @@
 package org.launchcode.TasteBuddiesServer.data;
 
 import org.launchcode.TasteBuddiesServer.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     public Optional<User> findByEmail(String email);
 
+
+    // this is a very simple implementation of a search, but that's all I need right now
+    // https://stackoverflow.com/questions/55707052/implement-search-functionality-for-jpa-and-spring-boot
+    @Query("SELECT n FROM user n WHERE " +
+            "(?1 upper(n.display_name) like concat('%', upper(?1), '%')) LIMIT 10")
+    public List<User> findDisplayNameFromFilter(String name);
 }
