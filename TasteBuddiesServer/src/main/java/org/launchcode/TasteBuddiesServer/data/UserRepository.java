643322dd1977409704d39ac50013c1a0afe5383b
Nathan <natiouswright@gmail.com>
2023-05-14 10:15:35 -0500

    fixed JPQL user search querry

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
@@ -14,7 +14,9 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     // this is a very simple implementation of a search, but that's all I need right now
     // https://stackoverflow.com/questions/55707052/implement-search-functionality-for-jpa-and-spring-boot
-    @Query("SELECT n FROM user n WHERE " +
-            "(?1 upper(n.display_name) like concat('%', upper(?1), '%')) LIMIT 10")
-    public List<User> findDisplayNameFromFilter(String name);
+//    @Query("SELECT n FROM user n WHERE " +
+//            "(?displayName upper(n.display_name) like concat('%', upper(?displayName), '%')) LIMIT 10")
+    @Query("SELECT displayName FROM User u WHERE (upper(u.displayName) LIKE concat('%', upper(:displayName), '%'))")
+//            "(upper(u.display_name) like concat('%', upper(:displayName), '%'))")
+    public List<User> findDisplayNameFromFilter(String displayName);
 }
