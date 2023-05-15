c435d71ca26b4a60e8476930935fe171d8e0e4c0
Ricky <ricky@ricky.com>
2023-05-15 05:28:30 -0500

    created form in event component to collect search input. Edited routing to show form

--- TasteBuddiesClient/src/app/app-routing.module.ts
+++ TasteBuddiesClient/src/app/app-routing.module.ts
@@ -7,14 +7,16 @@ import { AccountComponent } from './account/account.component';
 import { UserEventsComponent } from './user-events/user-events.component';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 import { AuthGuardService as AuthGuard } from 'src/services/auth-guard.service';
+import { EventComponent } from './event/event.component';
 
 const routes: Routes = [
+  { path: 'event/create', title: 'Create Event', component: EventComponent},
   { path: '', title: 'Taste Buddies', component: HomeComponent },
   { path: 'login', title: 'Login', component: LoginComponent },
   { path: 'signup', title: 'Register', component: RegistrationComponent },
   { path: 'account', title: 'Account', component: AccountComponent, canActivate: [AuthGuard] },
-  { path: 'event', title: 'Events', component: UserEventsComponent, canActivate: [AuthGuard] },
-  { path: '**', title: 'Page not found', component: PageNotFoundComponent },
+  { path: 'event', title: 'Events', component: UserEventsComponent, canActivate: [AuthGuard]},
+  { path: '**', title: 'Page not found', component: PageNotFoundComponent }
 ];
 
 @NgModule({
