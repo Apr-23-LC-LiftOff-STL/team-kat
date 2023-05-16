4345ce6703b86151dbf745b6637cc2b1159f49c2
Nathan-422 <natiouswright@gmail.com>
2023-05-16 16:10:47 -0500

    reset event component to init state

--- TasteBuddiesClient/src/app/events/event.component.ts
+++ TasteBuddiesClient/src/app/events/event.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Event } from 'src/models/event';
-import { EventService } from 'src/services/event.service';
 
 @Component({
   selector: 'app-event',
@@ -9,31 +6,10 @@ import { EventService } from 'src/services/event.service';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-  newEvent: Event = new Event;
-  submitted: boolean = false;
 
-  constructor(
-    private router: Router,
-    private eventService: EventService,
-    ) { }
+  constructor() { }
 
   ngOnInit(): void {
-
-  }
-
-  onSubmit(): void {
-    this.eventService.createEvent(this.newEvent).subscribe({
-      next: res => {
-        this.router.navigate(['/event']);
-      },
-      error: (e) => { 
-        console.error(e.message)
-      }
-    });
-  }
-
-  reloadPage(): void {
-    window.location.reload();
   }
 
 
