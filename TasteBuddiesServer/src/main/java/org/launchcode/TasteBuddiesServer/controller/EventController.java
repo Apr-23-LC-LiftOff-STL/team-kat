2583f696bf969dd15bdb58f5035d1b158c35f678
Ricky <ricky@ricky.com>
2023-05-16 23:13:21 -0500

    beggining of event controller post. new builder does not work currently

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/EventController.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/EventController.java
@@ -1,17 +1,21 @@
 package org.launchcode.TasteBuddiesServer.controller;
 
+import com.google.gson.Gson;
 import org.launchcode.TasteBuddiesServer.data.EventRepository;
 import org.launchcode.TasteBuddiesServer.data.RestaurantRepository;
 import org.launchcode.TasteBuddiesServer.data.UserRepository;
-import org.launchcode.TasteBuddiesServer.models.User;
 import org.launchcode.TasteBuddiesServer.models.dto.EventDTO;
 import org.launchcode.TasteBuddiesServer.models.geocode.TranscriptGC;
 import org.launchcode.TasteBuddiesServer.models.nearbySearch.TranscriptNB;
 import org.launchcode.TasteBuddiesServer.models.place.TranscriptPlace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
 
 @RestController
 @RequestMapping("/api/event")
@@ -23,12 +27,23 @@ public class EventController {
     private EventRepository eventRepository;
     @Autowired
     private RestaurantRepository restaurantRepository;
+    @Value("${apiKey}")
+    private String APIKey;
+//    @Autowired
+//    private Gson gson;
+
     @PostMapping("")
     public ResponseEntity<?> collectRestaurantData(@RequestBody EventDTO data){
         TranscriptGC transcriptGC = new TranscriptGC();
         TranscriptNB transcriptNB = new TranscriptNB();
         TranscriptPlace transcriptPlace = new TranscriptPlace();
-
+        String URLGC = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        String URLNB = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=restaurant&location=";
+        String URLPlace = "https://maps.googleapis.com/maps/api/place/details/json?place_id=";
+        Gson gson = new Gson();
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI()).build();
         return ResponseEntity.status(200).build();
     }
 }
