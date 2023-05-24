2eec405826aad1e7d256541ca09c8cd5baac04c2
Nathan-422 <natiouswright@gmail.com>
2023-05-24 15:24:25 -0500

    added dummy data to login and model tests

--- TasteBuddiesClient/src/models/login.spec.ts
+++ TasteBuddiesClient/src/models/login.spec.ts
@@ -2,6 +2,6 @@ import { Login } from './login';
 
 describe('Login', () => {
   it('should create an instance', () => {
-    expect(new Login()).toBeTruthy();
+    expect(new Login("test@test.com", "password")).toBeTruthy();
   });
 });
