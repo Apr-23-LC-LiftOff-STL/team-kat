e478195feb14b54d73bb69ef3288775924ab861c
Nathan-422 <natiouswright@gmail.com>
2023-05-24 16:15:40 -0500

    Cleaned up more files related to user search by displayName. And ran NPM isntall

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/api/APIUserController.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/controller/api/APIUserController.java
@@ -61,12 +61,4 @@ public class APIUserController {
         return ResponseEntity.status(200).body(user);
     }
 
-    @PostMapping("search")
-    public ResponseEntity<?> getUsersFromSearch(@RequestBody SearchDTO term) {
-        System.out.println(term.toString());
-        List<User> users = this.userRepository.findDisplayNameFromFilter(term.getTerm());
-        System.out.println(users.getClass());
-        return ResponseEntity.status(200).body(users);
-    }
-
 }
