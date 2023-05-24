388c529917023f1cc66f48bccf496841de2b8e72
Nathan-422 <natiouswright@gmail.com>
2023-05-24 15:33:13 -0500

    cleaned up commponents related to user displayname search

--- TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
+++ TasteBuddiesServer/src/main/java/org/launchcode/TasteBuddiesServer/data/UserRepository.java
@@ -1,18 +1,12 @@
 package org.launchcode.TasteBuddiesServer.data;
 
 import org.launchcode.TasteBuddiesServer.models.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     public Optional<User> findByEmail(String email);
 
-    // this is a very simple implementation of a search, but that's all I need right now
-    // https://stackoverflow.com/questions/55707052/implement-search-functionality-for-jpa-and-spring-boot
-    @Query("SELECT displayName FROM User u WHERE (upper(u.displayName) LIKE concat('%', upper(:displayName), '%'))")
-    public List<User> findDisplayNameFromFilter(String displayName);
 }
