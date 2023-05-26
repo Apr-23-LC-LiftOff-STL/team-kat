b535bdd11a9ac17259f91384cac43e55175981b8
Brad Rucker <bruck63@gmail.com>
2023-05-26 15:05:20 -0500

    Added the code to retrieve the list of restaurants from the googlePlacesService and load the javascript api from google maps.  Be sure to add your own api key to the loader object.

--- TasteBuddiesClient/src/app/view-restaurants/view-restaurants.component.ts
+++ TasteBuddiesClient/src/app/view-restaurants/view-restaurants.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GooglePlacesService } from '../../services/google-places.service';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-view-restaurants',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-restaurants.component.css']
 })
 export class ViewRestaurantsComponent implements OnInit {
+  restaurants: any[] = [];
 
-  constructor() { }
+  constructor(private googlePlacesService: GooglePlacesService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+    const loader = new Loader({
+      apiKey: '',
+      version: 'weekly',
+      libraries: ['places']
+    });
+
+    loader.load().then(() => {
+      // Google Maps JavaScript API loaded and initialized
+      // Call the Google Places service to fetch nearby places
+      this.fetchNearbyPlaces();
+    }).catch((error) => {
+      console.log('Failed to load Google Maps JavaScript API:', error);
+    })
+  }
+
+  fetchNearbyPlaces() {
+    //Parameters of getNearbyPlaces latLng: string, radius: string, keyWord: string
+    const latLng = "38.908, -90.043"; //Bethalto, IL Example
+    const radius = "2500";
+    const keyword = ""; //leave blank to call all restaurants in the area
+
+    // Call the Google Places service to fetch nearby places
+    this.googlePlacesService.getNearbyPlaces(latLng, radius, keyword)
+      .then(places => {
+        // Handle the returned places data as needed
+        this.restaurants = places.results;
+        console.log(this.restaurants);
+      })
+      .catch(error => {
+        // Handle the error if needed
+        console.log('Error occurred while fetching nearby places:', error);
+      });
+
   }
 
 }
