c3e565ec0c25f5513e955b7bc94adb014e421e6e
Nathan-422 <natiouswright@gmail.com>
2023-06-03 21:12:27 -0500

    Added places service to expose backend endpoint to frontend

---
+++ TasteBuddiesClient/src/services/places.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NewEventDTO } from 'src/models/DTO/new-event-dto';
+
+const EVENT_API = 'http://localhost:8080/api/places/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PlacesService {
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * 
+   * @returns 
+   */
+  public getRestaurantDetails(placeID: string):Observable<any> {
+    return this.http.get(
+      EVENT_API + 'restaurant?' +
+      'placeID=' + placeID
+    )
+  }
+
+  public getPhoto(
+    photoReference: string, 
+    maxwidth?: number, 
+    maxheight?: number,
+    ):Observable<any> 
+  {
+
+    if (maxwidth === undefined && maxheight === undefined) {
+      throw Error("Either or both maxwidth or maxheight are required");
+    }
+
+    let url = EVENT_API + 'photo?' +
+    'photo_reference=' + photoReference;
+
+    if (maxwidth! > 0) {
+      url += '&maxwidth=' + maxwidth;
+    }
+
+    if (maxwidth! > 0) {
+      url += '&maxheight=' + maxheight;
+    }
+
+    return this.http.get(url)
+
+  }
+
+}
