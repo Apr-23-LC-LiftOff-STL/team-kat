42a92951627337eeaf62576463671f7cc9dbb58f
Brad Rucker <bruck63@gmail.com>
2023-06-08 14:54:35 -0500

    Generated event-results component.

---
+++ TasteBuddiesClient/src/app/events/event-result/event-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-result',
+  templateUrl: './event-result.component.html',
+  styleUrls: ['./event-result.component.css']
+})
+export class EventResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
