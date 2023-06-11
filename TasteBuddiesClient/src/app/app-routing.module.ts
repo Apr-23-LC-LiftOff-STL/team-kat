ce37e95cf0d93e76b0129000fee44240359b2787
Belmonte's pc <belmontei9595@gmail.com>
2023-06-11 17:36:30 -0500

    proper merge

--- TasteBuddiesClient/src/app/app-routing.module.ts
+++ TasteBuddiesClient/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
-import { RegistrationComponent } from './auth/registration/registration.component'; 
+import { RegistrationComponent } from './auth/registration/registration.component';
 import { AccountComponent } from './account/account.component';
 import { UserEventsComponent } from './events/user-events/user-events.component';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
@@ -11,6 +11,7 @@ import { EventFormComponent } from './events/event-form/event-form.component';
 import { EventComponent } from './events/event.component';
 import { EventJoinComponent } from './events/event-join/event-join.component';
 import { EventResultComponent } from './events/event-result/event-result.component';
+import { EventPageComponent } from './events/event-page/event-page.component';
 
 const routes: Routes = [
   { path: '', title: 'Taste Buddies', component: HomeComponent },
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'event/create', title: 'New event', component: EventFormComponent, canActivate: [AuthGuard] },
   { path: 'event/:id', title: 'Event', component: EventComponent, canActivate: [AuthGuard] },
   { path: 'event', title: 'Events', component: UserEventsComponent, canActivate: [AuthGuard] },
+  { path: 'event-page', title: 'Event Page', component: EventPageComponent, canActivate:[AuthGuard]},
   { path: 'event/:id/result', component: EventResultComponent, canActivate: [AuthGuard] },
   { path: '**', title: 'Page not found', component: PageNotFoundComponent },
 ];
